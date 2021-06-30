41fb357c3a6440521b6d1fab0a27392ed9347550
sinthy <sumaiya15-4949@diu.edu.bd>
2021-06-30 22:39:17 +0200

    Implement API #9

--- routers/admin.js
+++ routers/admin.js
@@ -3,12 +3,25 @@ const { getDB } = require("../utils/db")
 
 const adminRouter = express.Router()
 
+// API #1 List all users
 adminRouter.get("/users", async function (req, res) {
   const dbResult = await getDB().any(
     "SELECT ID, USERNAME, FORENAME, SURNAME FROM PUPIL;"
   )
 
-  res.json(dbResult)
+  res.json({
+    users: dbResult,
+  })
+  res.end()
+})
+
+// API #9 List all classes
+adminRouter.get("/classes", async function (req, res) {
+  const dbResult = await getDB().any("SELECT NAME FROM CLASS;")
+
+  res.json({
+    classes: dbResult,
+  })
   res.end()
 })
 
