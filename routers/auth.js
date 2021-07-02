dc8cd5b48f20de91e890d45abe25bae6bb89bc2d
sinthy <sumaiya15-4949@diu.edu.bd>
2021-07-02 04:54:05 +0200

    Add rest api for logged in user

--- routers/auth.js
+++ routers/auth.js
@@ -5,9 +5,21 @@ const { getPasswordHash } = require("../utils/helpers")
 const authRouter = express.Router()
 
 authRouter.get("/me", async function (req, res) {
-  // console.log(req.headers)
-  res.json("hi me")
-  res.end()
+  try {
+    const rows = await dbq(
+      `SELECT ID, USERNAME, FORENAME, SURNAME FROM ${req.session.userRole.toUpperCase()} WHERE ID='${
+        req.session.userId
+      }'`
+    )
+    res.json({
+      ...rows[0],
+      role: req.session.userRole,
+    })
+    res.end()
+  } catch {
+    res.statusCode = 400
+    res.end("You are not logged in")
+  }
 })
 
 authRouter.post("/login", async function (req, res) {
@@ -22,15 +34,16 @@ authRouter.post("/login", async function (req, res) {
 
     if (rows.length === 0) {
       res.sendStatus(404)
-      res.end()
+      res.end("Invalid credentials")
       return
     }
 
     const loggedInUser = rows[0]
     const { id, surname, forename, username } = loggedInUser
 
-    req.session.isLoggedIn = true
-    req.session.loggedInUserId = id
+    req.session.userRole = req.body.role
+    req.session.userId = id
+    req.session.save()
 
     res.json({
       id,
